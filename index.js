b7be1ceded63da236c0ec41fca687b499b76325c
Andrew Goetz <agoetz4407@gmail.com>
2022-04-25 22:22:55 -0500

    email and github questions

--- index.js
+++ index.js
@@ -4,96 +4,124 @@ const generateMarkdown = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
 const questions = [
     {
-    type:'input',
-    name: 'title',
-    message: 'Enter your project title (required):',
-    validate: input => {
-        if (input) {
-            return true
-        } else {
-            console.log('Please enter a project title')
-            return false
+        type:'input',
+        name: 'title',
+        message: 'Enter your project title:',
+        validate: input => {
+            if (input) {
+                return true
+            } else {
+                console.log('Please enter a project title')
+                return false
+            }
         }
-    }
     },
     {
-    type:'input',
-    name: 'description',
-    message: 'Enter the description for your project (required):',
-    validate: input => {
-        if (input) {
-            return true
-        } else {
-            console.log('Please enter project description')
-            return false
+        type:'input',
+        name: 'description',
+        message: 'Enter the description for your project:',
+        validate: input => {
+            if (input) {
+                return true
+            } else {
+                console.log('Please enter project description')
+                return false
+            }
         }
-    }
     },
     {
-    type:'input',
-    name: 'installation',
-    message: 'Enter the installation instructions for your project (required):',
-    validate: input => {
-        if (input) {
-            return true
-        } else {
-            console.log('Please enter installation instructions for your project')
-            return false
+        type:'input',
+        name: 'installation',
+        message: 'Enter the installation instructions for your project:',
+        validate: input => {
+            if (input) {
+                return true
+            } else {
+                console.log('Please enter installation instructions for your project')
+                return false
+            }
         }
-    }
     },
     {
-    type:'input',
-    name: 'usage',
-    message: 'Enter the usage information for your project (required):',
-    validate: input => {
-        if (input) {
-            return true
-        } else {
-            console.log('Please enter usage information for your project')
-            return false
+        type:'input',
+        name: 'usage',
+        message: 'Enter the usage information for your project:',
+        validate: input => {
+            if (input) {
+                return true
+            } else {
+                console.log('Please enter usage information for your project')
+                return false
+            }
         }
-    }
     },
     {
-    type:'input',
-    name: 'contribution',
-    message: 'Enter contribution guidlines for your project (required):',
-    validate: input => {
-        if (input) {
-            return true
-        } else {
-            console.log('Please enter contribution guidlines for your project')
-            return false
+        type:'input',
+        name: 'contribution',
+        message: 'Enter contribution guidlines for your project:',
+        validate: input => {
+            if (input) {
+                return true
+            } else {
+                console.log('Please enter contribution guidlines for your project')
+                return false
+            }
         }
-    }
     },
     {
-    type:'input',
-    name: 'testing',
-    message: 'Enter testing instructions for your project (required):',
-    validate: input => {
-        if (input) {
-            return true
-        } else {
-            console.log('Please enter testing instructions for your project')
-            return false
+        type:'input',
+        name: 'testing',
+        message: 'Enter testing instructions for your project:',
+        validate: input => {
+            if (input) {
+                return true
+            } else {
+                console.log('Please enter testing instructions for your project')
+                return false
+            }
         }
-    }
     },
     {
-    type:'list',
-    name: 'license',
-    message: `Choose your license (or select "none" for no license):`,
-    defaut: 'none',
-    choices: [
-        {name:'MIT', value:'MIT'},
-        {name:'ISC', value:'ISC'},
-        {name:'Apache 2.0', value:'Apache_2.0'},
-        {name:'GNU GPLv3', value:'GNU_GPLv3'},
-        {name:'Mozilla Public License 2.0', value:'Mozilla_2.0'},
-        'none'
-    ],
+        type:'list',
+        name: 'license',
+        message: `Choose your license (or select "none" for no license):`,
+        defaut: 'none',
+        choices: [
+            {name:'MIT License', value:'MIT'},
+            {name:'ISC License', value:'ISC'},
+            {name:'Apache License 2.0', value:'Apache_2.0'},
+            {name:'GNU GPLv3', value:'GNU_GPLv3'},
+            {name:'Mozilla Public License 2.0', value:'Mozilla_2.0'},
+            'none'
+        ],
+    },
+    {
+        type:'input',
+        name: 'github',
+        message: 'Enter your Github username:',
+        validate: input => {
+            if (input) {
+                return true
+            } else {
+                console.log('Please enter your Github username')
+                return false
+            }
+        }
+    },
+    {
+        type:'input',
+        name: 'email',
+        message: 'Enter your email address:',
+        validate: input => {
+            const emailCheck = input.includes('@')
+            if (!input || !emailCheck) {
+                console.log(' Please enter a valid email address')
+                return false
+            }
+            else {
+                return true
+            }
+        }
     },
 ];
 
