e9cdcf2f654e6682c3f582dd09d4654bcb96c4ff
Andrew Goetz <agoetz4407@gmail.com>
2022-04-25 20:17:07 -0500

    questions updated

--- index.js
+++ index.js
@@ -1,13 +1,96 @@
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    type:'input',
+    name: 'title',
+    message: 'Enter your project title (required):',
+    validate: input => {
+        if (input) {
+            return true
+        } else {
+            console.log('Please enter a project title')
+            return false
+        }
+    }
+    },
+    {
+    type:'input',
+    name: 'description',
+    message: 'Enter the description for your project (required):',
+    validate: input => {
+        if (input) {
+            return true
+        } else {
+            console.log('Please enter project description')
+            return false
+        }
+    }
+    },
+    {
+    type:'input',
+    name: 'installation',
+    message: 'Enter the installation instructions for your project (required):',
+    validate: input => {
+        if (input) {
+            return true
+        } else {
+            console.log('Please enter installation instructions for your project')
+            return false
+        }
+    }
+    },
+    {
+    type:'input',
+    name: 'usage',
+    message: 'Enter the usage information for your project (required):',
+    validate: input => {
+        if (input) {
+            return true
+        } else {
+            console.log('Please enter usage information for your project')
+            return false
+        }
+    }
+    },
+    {
+    type:'input',
+    name: 'contribution',
+    message: 'Enter contribution guidlines for your project (required):',
+    validate: input => {
+        if (input) {
+            return true
+        } else {
+            console.log('Please enter contribution guidlines for your project')
+            return false
+        }
+    }
+    },
+    {
+    type:'input',
+    name: 'testing',
+    message: 'Enter testing instructions for your project (required):',
+    validate: input => {
+        if (input) {
+            return true
+        } else {
+            console.log('Please enter testing instructions for your project')
+            return false
+        }
+    }
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt()
+
+}
 
 // Function call to initialize app
 init();
