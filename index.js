009290322cdead9ab17bf4c136cef190761535df
Andrew Goetz <agoetz4407@gmail.com>
2022-04-27 00:28:08 -0500

    template with license info

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const questions = [
     {
         type:'input',
         name: 'contribution',
-        message: 'Enter contribution guidlines for your project:',
+        message: 'Enter contribution guidelines for your project:',
         validate: input => {
             if (input) {
                 return true
@@ -87,12 +87,54 @@ const questions = [
         message: `Choose your license (or select "none" for no license):`,
         defaut: 'none',
         choices: [
-            'MIT License',
-            'ISC License',
-            'Apache License 2.0',
-            'GNU GPLv3',
-            'Mozilla Public License 2.0',
-            'Boost Software License 1.0',
+            {
+                name:'MIT License', 
+                value: {
+                    name:'MIT License',
+                    badge:'MIT',
+                    link:'https://opensource.org/licenses/MIT'
+                }
+            },
+            {
+                name:'ISC License',
+                value: {
+                    name:'ISC License',
+                    badge:'ISC',
+                    link:'https://opensource.org/licenses/ISC'
+                }
+            },
+            {
+                name:'Apache License 2.0',
+                value: {
+                    name:'Apache License 2.0',
+                    badge:'Apache_2.0',
+                    link:'https://www.apache.org/licenses/LICENSE-2.0'
+                }
+            },
+            {
+                name:'GNU GPLv3',
+                value: {
+                    name:'GNU General Public License v3.0',
+                    badge:'GNU_GPLv3',
+                    link:'https://www.gnu.org/licenses/gpl-3.0.en.html'
+                }
+            },
+            {
+                name:'Mozilla Public License 2.0',
+                value: {
+                    name:'Mozilla Public License 2.0',
+                    badge:'MPL_2.0',
+                    link:'https://www.mozilla.org/en-US/MPL/2.0/'
+                }
+            },
+            {
+                name:'Boost Software License 1.0',
+                value: {
+                    name:'Boost Software License 1.0',
+                    badge:'Boost_1.0',
+                    link:'https://www.boost.org/LICENSE_1_0.txt'
+                }
+            },
             'none'
         ],
     },
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,35 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "none") {
+    return ""
+  }
+  return `[![License](https://img.shields.io/badge/License-${license.badge}-blue.svg)](${license.link})`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "none") {
+    return ""
+  }
+  return '- [License](#License)' 
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "none") {
+    return ""
+  }
+  return `## License
+  [${license.name}](${license.link})`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+${renderLicenseBadge(data.license)}
 ## Description
 ${data.description}
 
@@ -24,6 +40,7 @@ ${data.description}
 - [Contributing](#Contributing)
 - [Tests](#Tests)
 - [Questions](#Questions)
+${renderLicenseLink(data.license)}
 
 ## Installation
 ${data.installation}
@@ -38,8 +55,10 @@ ${data.contribution}
 ${data.testing}
 
 ## Questions
-Check out my [Github]https://github.com/${data.github}
+Check out my [Github](https://github.com/${data.github})
 Send me an email with your questions at [${data.email}](mailto:${data.email})
+
+${renderLicenseSection(data.license)}
 `;
 }
 
